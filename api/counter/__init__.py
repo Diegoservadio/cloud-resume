bdb457cbdca1add41c92a4ec3f7d79e771290ded
Diego Servadio <servadiodiego7@gmail.com>
2026-06-08 17:29:47 +0200

    debug: check env vars

--- api/counter/__init__.py
+++ api/counter/__init__.py
@@ -1,7 +1,6 @@
 ﻿import azure.functions as func
 import json
 import os
-from azure.cosmos import CosmosClient, exceptions
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     headers = {
@@ -9,37 +8,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         "Content-Type": "application/json"
     }
 
-    if req.method == "OPTIONS":
-        return func.HttpResponse(status_code=204, headers=headers)
+    endpoint = os.environ.get("COSMOS_ENDPOINT", "MANCANTE")
+    key = os.environ.get("COSMOS_KEY", "MANCANTE")
 
-    try:
-        # Legge le variabili d'ambiente impostate su Azure
-        endpoint = os.environ["COSMOS_ENDPOINT"]
-        key      = os.environ["COSMOS_KEY"]
-        
-        client   = CosmosClient(endpoint, key)
-        db       = client.get_database_client("ResumeDB")
-        container = db.get_container_client("Counter")
-
-        # Legge il documento contatore
-        try:
-            item = container.read_item(item="counter", partition_key="counter")
-            item["count"] += 1
-        except exceptions.CosmosResourceNotFoundError:
-            item = {"id": "counter", "count": 1}
-
-        # Salva il documento aggiornato
-        container.upsert_item(item)
-
-        return func.HttpResponse(
-            json.dumps({"count": item["count"]}),
-            status_code=200,
-            headers=headers
-        )
-
-    except Exception as e:
-        return func.HttpResponse(
-            json.dumps({"count": 0, "error": str(e)}),
-            status_code=500,
-            headers=headers
-        )
+    # Debug temporaneo: restituisce le variabili (prime 10 lettere)
+    return func.HttpResponse(
+        json.dumps({
+            "endpoint_preview": endpoint[:30],
+            "key_found": len(key) > 10
+        }),
+        status_code=200,
+        headers=headers
+    )
